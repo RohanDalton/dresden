245224c7fd91f6c3ef72eafcfb8c304efb5bb3c2
Rohan Dalton <rohan@rohandalton.io>
2021-02-24 02:42:58 -0500

    Added a unit test.

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "Rohan B. Dalton"
+
+
+if __name__ == "__main__":
+    pass
---
+++ test/test_harry.py
@@ -0,0 +1,12 @@
+from dresden import Harry
+
+__author__ = "Rohan B Dalton"
+
+
+def test_context_manager():
+    with Harry():
+        pass
+
+
+if __name__ == "__main__":
+    pass
